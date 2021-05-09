963b344193edef649627b0bf10987950ca0d56a7
volga52 <volga52@bk.ru>
2021-05-09 20:42:40 +0300

    change mainapp.models and recovery database form fixtures

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
 
-
-# Create your models here.
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
@@ -17,9 +13,14 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=256)
-    image = models.ImageField(upload_to='product_images', blank=True)
-    description = models.CharField(max_length=64, blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя продукта', max_length=128)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
+    description = models.TextField(verbose_name='описание продукта', blank=True)
+    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
