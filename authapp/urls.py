08a736a10357afa3e466540b885afc56655d0556
volga52 <volga52@bk.ru>
2021-05-09 21:19:16 +0300

    Task 2-1 make first-menu authentication

---
+++ authapp/urls.py
@@ -0,0 +1,9 @@
+﻿from django.urls import path
+
+from authapp.views import login
+
+app_name = 'authapp'
+
+urlpatterns = [
+    path('login/', login, name='login'),
+]
--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    context = {'title': 'GeekShop - Авторизация'}
+    return render(request, 'authapp/login.html', context)
