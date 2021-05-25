dd21c71d0bd3740d940b655214a5e46885193d56
volga52 <volga52@bk.ru>
2021-05-25 15:34:18 +0300

    Task 2_1 create admin-users-read

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,10 +1,11 @@
 ﻿from django.urls import path
 
-from adminapp.views import index
+from adminapp.views import index, admin_user_read
 
 
 app_name = 'adminapp'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('admin-users-read/', admin_user_read, name='admin_users_read'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from authapp.models import User
 
 def index(request):
     return render(request, 'adminapp/admin.html')
+
+
+def admin_user_read(request):
+    context = {'users': User.objects.all()}
+    return render(request, 'adminapp/admin-users-read.html', context)
