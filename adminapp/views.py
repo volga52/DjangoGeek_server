469f8675ae36c49338aa75cf275b45400f28817b
volga52 <volga52@bk.ru>
2021-05-25 20:12:21 +0300

    Task 3 decor user_passes_test

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegistrerForm, UserAdminProfileForm
 
+
+@user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return render(request, 'adminapp/admin.html')
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_user_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
 
+
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrerForm(data=request.POST, files=request.FILES)
@@ -24,6 +30,7 @@ def admin_users_create(request):
     return render(request, 'adminapp/admin-users-create.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_update(request, user_id):
     selected_user = User.objects.get(id=user_id)
     if request.method == 'POST':
@@ -37,6 +44,7 @@ def admin_users_update(request, user_id):
     return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_remove(request, user_id):
     user = User.objects.get(id=user_id)
     user.is_active = False
