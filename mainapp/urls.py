65978f701d0b08f2edaa6604d11fd2fc11fdf097
volga52 <volga52@bk.ru>
2021-05-26 14:05:51 +0300

    Task 2 create paginator-method

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,4 +7,5 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', products, name='index'),
     path('<int:category_id>/', products, name='product'),
+    path('page/<int:page>/', products, name='page'),
 ]
